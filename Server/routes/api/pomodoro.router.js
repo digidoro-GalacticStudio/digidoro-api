b25942a26d6978c870f095cc60b441df36d663c4
HenryLima07 <00033721@uca.edu.sv>
2023-04-07 20:33:07 -0600

    :hammer: update crud route

--- Server/routes/api/pomodoro.router.js
+++ Server/routes/api/pomodoro.router.js
@@ -5,10 +5,31 @@ const pomodoroController = require("../../controllers/pomodoro.controller");
 
 const pomodroValidators = require("../../validators/pomodoro.validator");
 const runValidations = require("../../validators/index.middleware");
-const userController = require("../../controllers/user.controller");
 
 router.get("/", pomodoroController.getAll);
 
-router.get("/:id", )
+router.get("/:id", 
+    pomodroValidators.findPomodoroByIdValidator,
+    runValidations,
+    pomodoroController.getById
+);
+
+router.post("/",
+    pomodroValidators.createPomodoroValidator,
+    runValidations,
+    pomodoroController.create
+);
+
+router.patch("/:id", 
+    pomodroValidators.findPomodoroByIdValidator,
+    runValidations,
+    pomodoroController.updateById
+);
+
+router.delete("/:id",
+    pomodroValidators.findPomodoroByIdValidator,
+    runValidations,
+    pomodoroController.deleteById
+);
 
 module.exports = router;
