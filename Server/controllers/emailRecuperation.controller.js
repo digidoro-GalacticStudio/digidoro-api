6cc3911409e66e32afa661f48cea534a95c1a811
HenryLima07 <00033721@uca.edu.sv>
2023-04-26 18:37:29 -0600

    :construction: allow sending recovery emails

--- Server/controllers/emailRecuperation.controller.js
+++ Server/controllers/emailRecuperation.controller.js
@@ -10,8 +10,8 @@ const config = {
 
     service: "gmail",
     host: "smt.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth:{
         user: process.env.DIGIDORO_EMAIL || "galactic.studio23@gmail.com",
         pass: process.env.DIGIDORO_PASS || ""
@@ -24,13 +24,15 @@ favoriteNoteController.emailRecuperation = async(req, res) =>{
     try{
         const from = process.env.DIGIDORO_EMAIL || "galactic.studio23@gmail.com";
         const subject = "DIGIDORO account recovery"
-        const text = `Hi there, keep on using DIGIDORO to help on your time. Click on the following Link --> ${"link"} to recover your account`
+        //const text = `Hi there, keep on using DIGIDORO to help on your time. Click on the following Link --> ${"link"} to recover your account`
         const { email } = req.body;
+        const HTML = '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>';
         const data = {
-            from,
-            email,
-            subject,
-            text,
+            from: from,
+            to: email,
+            subject: subject,
+            //text: text,
+            html: HTML
         }
         const transport = nodemailer.createTransport(config);
         transport.sendMail(data, (err, info) =>{
