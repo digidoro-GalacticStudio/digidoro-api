a83825ff5015e0c27375e1ac56e6a7cb01854b3b
MelvinAguilar <00067621@uca.edu.sv>
2023-07-01 14:38:32 -0600

    Update pomodoro model

--- Server/models/pomodoro.model.js
+++ Server/models/pomodoro.model.js
@@ -3,49 +3,30 @@ const Schema = mongoose.Schema;
 
 const pomodoroSchema = new Schema(
   {
-    name:{
+    name: {
       type: String,
       trim: true,
-      default: "New Pomodoro"
+      default: "New Pomodoro",
     },
-    user_id:{
+    user_id: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
     },
-    pomodoro: {
+    sessions_completed: {
       type: Number,
-      default: 25,
-      min: 1,
+      default: 0,
+      min: 0,
     },
-    break: {
+    total_sessions: {
       type: Number,
-      default: 5,
-      min: 1,
-    },
-    long_break: {
-      type: Number,
-      default: 15,
-      min: 1,
-    },
-    break_pomodoro: {
-      type: Number,
-      default: 4,
-      min: 1,
-    },
-    pomodoro_day: {
-      type: Number,
-      default: 8,
+      default: 1,
       min: 1,
     },
     theme: {
       type: String,
       trim: true,
       default: "#FFFFFF",
-    },
-    reminder: {
-      type: Boolean,
-      default: true,
     },
   },
   { timestamps: true }
--- Server/validators/pomodoro.validator.js
+++ Server/validators/pomodoro.validator.js
@@ -2,52 +2,27 @@ const { body, param } = require("express-validator");
 
 const createPomodoroValidator = [
   body("user_id")
-  .optional()
-  .isMongoId()
-  .withMessage("user_id must be a non-empty ID"),
-  body("pomodoro")
+    .optional()
+    .isMongoId()
+    .withMessage("user_id must be a non-empty ID"),
+  body("sessions_completed")
     .optional()
     .isInt()
-    .withMessage("pomodoro must be Integer")
+    .withMessage("sessions_completed must be Integer")
     .bail()
-    .isInt({ min: 1 })
-    .withMessage("pomodoro must be greater than 1"),
-  body("break")
+    .isInt({ min: 0 })
+    .withMessage("sessions_completed must be greater than 1"),
+  body("total_sessions")
     .optional()
     .isInt()
-    .withMessage("break must be Integer")
+    .withMessage("total_sessions must be Integer")
     .bail()
     .isInt({ min: 1 })
-    .withMessage("break must be greater than 1"),
-  body("long_break")
-    .optional()
-    .isInt()
-    .withMessage("long_break must be Integer")
-    .bail()
-    .isInt({ min: 1 })
-    .withMessage("long_break must be greater than 1"),
-  body("break_pomodoro")
-    .optional()
-    .isInt()
-    .withMessage("break_pomodoro must be Integer")
-    .bail()
-    .isInt({ min: 1 })
-    .withMessage("break_pomodoro must be greater than 1"),
-  body("pomodoro_day")
-    .optional()
-    .isInt()
-    .withMessage("pomodoro_day must be Integer")
-    .bail()
-    .isInt({ min: 1 })
-    .withMessage("pomodoro_day must be greater than 1"),
+    .withMessage("total_sessions must be greater than 1"),
   body("theme")
     .optional()
     .isHexColor()
     .withMessage("Color must be a valid hex color code"),
-  body("reminder")
-    .optional()
-    .isBoolean()
-    .withMessage("reminder must be Boolean"),
 ];
 
 const findPomodoroByIdValidator = [
@@ -58,7 +33,7 @@ const findPomodoroByIdValidator = [
     .withMessage("Invalid pomodoro ID"),
 ];
 
-const changeThemeValidator = [  
+const changeThemeValidator = [
   body("theme")
     .notEmpty()
     .withMessage("The theme required")
